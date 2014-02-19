451166ebcf85f0f4f19e633a9c4c09665492a3dc
Mustafa Pamuk <pamukmustafa@hotmail.de>
2014-02-19 11:45:43 +0100

    JavaScript für TouchDevices

--- Startseite-Dateien/script.js
+++ Startseite-Dateien/script.js
@@ -99,4 +99,62 @@ jQuery(function() {
 
 	jQuery('.external-link-new-window').attr('target', '_blank');
 
+
+	/**
+	* Tool mode stuff
+	*/
+
+	/* closes the tool mode menu */
+	function closeToolMenu() {
+		jQuery('.nav-menu ul').hide();
+	}
+
+	/**
+	* opens the tool mode menu if the menu button is clicked
+	* required for mouse and touch devices
+	*/
+	jQuery('.nav-menu').mouseenter(function() {
+		jQuery('.nav-menu ul').show();
+	});
+
+	/**
+	* closes the tool mode menu if the main page content is clicked
+	* required for touch devices
+	*/
+	jQuery('.main').click(function(){
+		closeToolMenu();
+	});
+
+	/**
+	* closes the tool mode menu if the breadcrumb is clicked
+	* required for touch devices
+	*/
+	jQuery('.nav-breadcrumb').click(function(){
+		closeToolMenu();
+	});	
+
+	/**
+	* closes the tool mode menu if mouse leaves the tool mode menu
+	* required for mouse devices
+	*/
+	jQuery('.nav-menu').mouseleave(function() {
+		closeToolMenu();
+	});
+
+	/**
+	* open/closes the tool mode menu
+	* required for mouse and touch devices
+	*/
+	jQuery('.nav-menu').click(function() {
+		if (jQuery('.nav-menu ul').is(':visible')) {
+			closeToolMenu();
+		} else {
+			jQuery('.nav-menu ul').show();
+		}
+	});
+
+	/* Tool mode | End */
+
+
+
 });
